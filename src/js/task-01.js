250efcd0018bb44980255843677ae182c2f2e835
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-06 23:13:33 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -5,5 +5,5 @@ const items = document.querySelectorAll(".item");
 Array.prototype.forEach.call(items, (element) => {
   const title = element.querySelector("h2").innerHTML;
   const itemsLength = element.querySelectorAll("li").length;
-  console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
+  console.log(` Category: ${title}. \n Elements: ${itemsLength}.`);
 });
