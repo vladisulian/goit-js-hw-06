80e9f59eaa28de8d45015e37dea9d2fc4bee2b76
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-06 23:10:00 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,9 @@
-const liItem = document.querySelectorAll(".item").length;
-console.log ("Number of categories: " + liItem)
+const item = document.querySelectorAll("#categories .item").length; // or I can use just .item
+console.log("Number of categories: " + item);
+
+const items = document.querySelectorAll(".item");
+Array.prototype.forEach.call(items, (element) => {
+  const title = element.querySelector("h2").innerHTML;
+  const itemsLength = element.querySelectorAll("li").length;
+  console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
+});
