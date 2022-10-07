12ea1403d7426942b797e7748404cf309984accb
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-07 17:38:22 +0300

    second task is done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,7 +7,14 @@ const ingredients = [
   "Condiments",
 ];
 
+// marked #ingredients
+const ingredientsList = document.querySelector("#ingredients");
+console.log(ingredientsList);
+
 ingredients.forEach(function (ingredients) {
   const listItem = document.createElement("li");
+  listItem.textContent = ingredients;
+  listItem.classList.add("item");
   console.log(listItem);
+  ingredientsList.appendChild(listItem);
 });
