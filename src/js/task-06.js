d9b127426f76a8c1753ec231e54a8480d93a7391
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-08 23:29:44 +0300

    Update task n. 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,31 +1,29 @@
 const inputEl = document.querySelector("#validation-input");
 const h = document.createElement("h1");
 const p = document.createElement("p");
-p.textContent =
-  "Можно ввести пустой инпут, и он пройдёт валидацию, если пробелов будет больше чем 6. Если меньше - то не засчитывает. Как решить проблему?";
-document.body.append(p);
+document.body.append(h);
+// p.textContent =
+//   "Можно ввести пустой инпут, и он пройдёт валидацию, если пробелов будет больше чем 6. Если меньше - то не засчитывает. Как решить проблему?";
+// document.body.append(p);
 function inputValidationCheck() {
-  if (inputEl.value.length >= 6) {
+  if (inputEl.value.length >= 6 && !inputEl.value.includes(" ")) {
     inputEl.classList.add("valid");
     inputEl.classList.remove("invalid");
 
     h.textContent = "Input is valid";
     h.style.color = "green";
-    document.body.append(h);
   } else if (inputEl.value.includes(" ")) {
     inputEl.classList.add("invalid");
     inputEl.classList.remove("valid");
 
-    h.textContent = "Нужно убрать пробелы";
+    h.textContent = "We need to remove empty spaces!";
     h.style.color = "orange";
-    document.body.append(h);
   } else {
     inputEl.classList.add("invalid");
     inputEl.classList.remove("valid");
 
     h.textContent = "Input is invalid!";
     h.style.color = "red";
-    document.body.append(h);
   }
 }
 
