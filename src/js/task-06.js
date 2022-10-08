869e050bee6992b847d76bd1136dc615197322f1
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-08 21:31:49 +0300

    Edit task n. 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,12 +1,31 @@
 const inputEl = document.querySelector("#validation-input");
-
+const h = document.createElement("h1");
+const p = document.createElement("p");
+p.textContent =
+  "Можно ввести пустой инпут, и он пройдёт валидацию, если пробелов будет больше чем 6. Если меньше - то не засчитывает. Как решить проблему?";
+document.body.append(p);
 function inputValidationCheck() {
   if (inputEl.value.length >= 6) {
     inputEl.classList.add("valid");
-    console.log("Input is valid");
+    inputEl.classList.remove("invalid");
+
+    h.textContent = "Input is valid";
+    h.style.color = "green";
+    document.body.append(h);
+  } else if (inputEl.value.includes(" ")) {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+
+    h.textContent = "Нужно убрать пробелы";
+    h.style.color = "orange";
+    document.body.append(h);
   } else {
     inputEl.classList.add("invalid");
-    console.log("Input is invalid");
+    inputEl.classList.remove("valid");
+
+    h.textContent = "Input is invalid!";
+    h.style.color = "red";
+    document.body.append(h);
   }
 }
 
