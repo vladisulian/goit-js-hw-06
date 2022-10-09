b2f04fce7b74d66b61edcbaaabd5c5dc4ac376c9
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-09 14:19:22 +0300

    Task n. 8 is done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,2 +1,27 @@
-const formEl = document.querySelector("login-form");
-console.log(formEl);
+// form el
+const form = document.querySelector(".login-form");
+const email = form[0];
+const pass = form[1];
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (email.value === "" || pass.value === "") {
+    alert("All fields must be filled");
+  } else {
+    // formData.forEach((value, name) => {
+    //   const formData = new FormData(event.currentTarget);
+    //   console.log("onFormSubmit -> name", name);
+    //   console.log("onFormSubmit -> value", value);
+    // });
+    const formElements = event.currentTarget.elements;
+    const mailValue = formElements.email.value;
+    const passwordValue = formElements.password.value;
+
+    console.log("Mail:", mailValue, "\n", "Password:", passwordValue);
+  }
+
+  form.reset();
+}
