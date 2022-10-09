1bff852fc3876500429b2509640cdc39a79769b1
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-09 16:49:24 +0300

    Task n. 9 is done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const changeColorButton = document.querySelector(".change-color");
+const span = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  span.textContent = document.body.style.backgroundColor;
 }
