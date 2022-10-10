e54d6ed0cc945afe6137f280522f4dea88a9b1ea
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-11 02:32:45 +0300

    Rewrite task n. 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-const item = document.querySelectorAll("#categories .item").length; // or I can use just .item
-console.log("Number of categories: " + item);
+const itemLength = document.querySelectorAll("#categories .item").length; // or I can use just .item
+console.log("Number of categories: " + itemLength);
 
 const items = document.querySelectorAll(".item");
-Array.prototype.forEach.call(items, (element) => {
+items.forEach((element) => {
   const title = element.querySelector("h2").innerHTML;
   const itemsLength = element.querySelectorAll("li").length;
   console.log(` Category: ${title}. \n Elements: ${itemsLength}.`);
